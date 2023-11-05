7fcc976e3c13888bcf17ff1d6517a2d2be4f4cfc
pratiknilopant <mr.pratiknilopant@gmail.com>
2023-11-05 18:57:39 +0530

    final commit

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -1,5 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-function UserTable() {}
+const UserTable = () => {
+  const [userData, setUserData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-export default UserTable;
+  useEffect(() => {
+    // Fetch user data from the API
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => {
+        // Sort the data by ID
+        data.sort((a, b) => a.id - b.id);
+        setUserData(data);
+        setLoading(false);
+      })
+      .catch((error) => console.error('Error fetching user data: ', error));
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Website</th>
+        </tr>
+      </thead>
+      <tbody>
+        {userData.map((user) => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.phone}</td>
+            <td>{user.website}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default UserTable;
